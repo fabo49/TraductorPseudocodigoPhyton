de6ceb26bfb485434e8a0feefc2807322f23dedc
Fabian Rodriguez <farodriguez.49@gmail.com>
2015-07-04 09:49:43 -0600

    pequeños cambios de la gramática

--- proyecto.py
+++ proyecto.py
@@ -9,124 +9,89 @@
 # -----------------------------------------------------------
 
 # lexer
+import ply.lex as lex
+import ply.yacc as yacc
 
-tokens = ('COMA', 'VAR', 'FIN', 'IF', 'WHILE', 'FOR', 'ELSE', 'IN', 'PARENA', 'PARENC', 'OPBIN', 'OPUNA', 'ASK', 'PRINT', 'ASIG', 'NUM', 'ID', 'PROC', 'EXPBOOL', 'COMENT', 'TAB', 'STRING', 'RETURN')
+tokens = ['ID', 'COMA', 'VAR', 'PARENA', 'PARENC', 'OPBIN', 'OPUNA', 'ASK', 'PRINT', 'ASIG', 'NUM', 'FIN', 'PROC', 'EXPBOOL', 'COMENT', 'STRING', 'RETURN']
+
+reserved = {
+	'mientras' : 'WHILE',
+	'si no' : 'ELSE',
+	'para cada' : 'FOR',
+	'en' : 'IN',
+	'si' : 'IF'
+}
+tokens += reserved.values()
 
 m_tabs = 0 	# variable global que lleva cuantos tabs se hicieron en cada linea
 m_result = ""
 m_totLineas = 1
 m_reporte = ""
 
-def t_coma(t):
-	r','
+t_COMA = r','
+t_FIN = r'fin|Fin|FIN'
+t_VAR = r'var|Var|VAR'
+t_PARENA = r'\('
+t_PARENC = r'\)'
+t_OPUNA = r'\+\+|--'
+t_ASIG = r'=|\+=|-=|\*=|/='
+t_STRING = r'\'[^\n\r]\''
+
+def t_ID(t):
+	r'[_a-zA-Z][_a-zA-Z0-9]*'
+	t.type = reserved.get(t.value,'ID')		# busca en la lista de palabras reservadas y si no esta, lo asigna como un ID
 	return t
 
-def t_fin(t):
-	r'fin'
-	t.value = ''
-	return t
-
-def t_var(t):
-	r'var'
-	t.value = ''
-	return t
-
-def t_else(t):
-	r'si no'
-	t.value = 'else'
-	return t
-
-def t_while(t):
-	r'mientras'
-	t.value = 'while'
-	return t
-
-def t_for(t):
-	r'para cada'
-	t.value = 'for'
-	return t
-
-def t_in(t):
-	r'en'
-	t.value = 'in'
-	return t
-
-def t_if(t):
-	r'si'
-	t.value = 'if';
-	return t
-
-def t_parena(t):
-	r'\('
-	return t
-
-def t_parenc(t):
-	r'\)'
-	return t
-
-def t_opbin(t):
+def t_OPBIN(t):
 	r'\+|-|\*|/|mod|==|!=|>=|<=|>|<|y|o'
 	if t.value == 'mod':
 		t.value = '%'
+		return t
 	elif t.value == 'y':
 		t.value = 'and'
+		return t
 	elif t.value == 'o':
 		t.value = 'or'
+		return t
 	return t
 
-def t_opuna(t):
-	r'\+\+|--'
-	return t
-
-def t_ask(t):
+def t_ASK(t):
 	r'preguntar'
 	t.value = 'input'
 	return t
 
-def t_print(t):
+def t_PRINT(t):
 	r'imprimir'
 	t.value = 'print'
 	return t
 
-def t_asig(t):
-	r'=|\+=|-=|\*=|/='
-	return t
-
-def t_num(t):
+def t_NUM(t):
 	r'\d+'
 	t.value = int(t.value)
 	return t
 
-def t_id(t):
-	r'[a-zA-Z_][0-9a-zA-Z_]*'
-	return t
-
-def t_proc(t):
+def t_PROC(t):
 	r'proceso'
 	t.value = 'def'
 	return t
 
-def t_expbool(t):
+def t_EXPBOOL(t):
 	r'verdadero|falso'
 	if t.value == 'verdadero':
 		t.value = 'True'
+		return t
 	else:
 		t.value = "False"
+		return t
 
-	return t
-
-def t_return(t):
+def t_RETURN(t):
 	r'retornar'
 	t.value = 'return'
 	return t
 
-def t_coment(t):
+def t_COMENT(t):
 	r'<--'
 	t.value = '#'
-	return t
-
-def t_tab(t):
-	r'(\t)+'
 	return t
 
 # Hace el cambio de linea
@@ -135,24 +100,23 @@ def t_newline(t):
 	global m_totLineas
 	m_totLineas += 1
 
-def t_string(t):	# funciona tanto para comentarios como para strings
-	r'[^\n\r]'
-	return t
-
 def t_error (t):
     print ("Error lexico")
     t.lexer.skip(1)
 
-# Metodo auxiliar que se encarga de hacer la identacion de cada linea en python
-def add_tabs(cantTabs):
-	nivel = ""
-	for i in cantTabs:	# hace tantos tabs como lo dice el parametro que recibe
-		nivel += '\t'
-	nivel = '\n'+nivel	# agrega el cambio de linea
-	return nivel
+#ignora espacios en blanco
+t_ignore = " \t"
 
-import ply.lex as lex
 lex.lex()
+
+
+# Metodo auxiliar que se encarga de hacer la identacion de cada linea en python
+def add_tabs():
+	global m_tabs
+	nivel = '\n'		# para hacer el cambio de linea despues de la instruccion
+	for i in m_tabs:	# agrega tantos tabs como lo inque m_tabs
+		nivel += '\t'
+	return nivel
 
 # parser
 
@@ -162,7 +126,6 @@ lex.lex()
 #	* parametro
 #	* parametros
 #		- puede ser 0, 1 o "n" parametros
-#	* tabs
 #	* comentarios
 #	* operaciones
 #		- operaciones aritmeticas
@@ -180,26 +143,15 @@ def p_root(p):
 	'''root : inicio'''
 	p[0] = p[1]
 	global m_result
-	m_result = p[0]
-	file = open('output.py', 'w')
-	file.write(m_result)
-	file.close()
+	m_result = str(p[0])
 
 def p_inicio(p):
 	'''inicio : instruccion inicio'''
 	p[0] = p[1] + p[2]
 
 def p_inicio2(p):
-	'''inicio : instruccion '''
+	'''inicio : instruccion fininstru'''
 	p[0] = p[1]
-
-def p_tabs(p):
-	'''tabs : TAB '''
-	global m_tabs
-	m_tabs = len(str(p[1]))		# cada vez que encuentra un tab incrementa la variable, cada vez que se hace un cambio de linea hay que ponerla en 0 otra vez
-	addTabs = add_tabs(m_tabs)
-	m_tabs = 0
-	p[0] = addTabs
 
 # reconoce epsilon
 def p_empty(p):
@@ -208,7 +160,12 @@ def p_empty(p):
 
 def p_fin(p):
 	'''fininstru : FIN'''
-	p[0] = p[1]
+	p[0] = ''
+	global m_tabs
+	if m_tabs > 0:
+		m_tabs -= 1
+	else:
+		m_tabs = 0
 
 # para reconocer tanto numeros solos, ids solos, expresiones booleanas (True/False) solas o strings
 def p_inmediato(p):
@@ -217,41 +174,24 @@ def p_inmediato(p):
 				 | EXPBOOL'''
 	p[0] = p[1]
 
-# reconoce la estructura de un "input" en el pseudocodigo
-def p_preguntar(p):
-	'''preguntar : tabs ID ASIG ASK PARENA STRING PARENC '''
-	p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7]
-
 # reconoce tanto operaciones aritmeticas como logicas en el pseudocodigo
 def p_operacion(p):
-	'''operacion : PARENA operacion PARENC operacion2'''
-	p[0] = p[1] + p[2] + p[3] + p[4]
-
-def p_operacion1(p):
-	'''operacion : inmediato operacion2'''
+	''' operacion : operacion1'''
 	p[0] = p[1]
-def p_operacion2(p):
-	'''operacion2 : OPBIN operacion operacion3'''
+
+def p_operacion_normal_parentesis(p):
+	''' operacion1 : operacion OPBIN operacion
+				   | PARENA operacion PARENC'''
 	p[0] = p[1] + p[2] + p[3]
 
-def p_operacion3(p):
-	'''operacion3 : operacion2
-				  | empty'''
+def p_operacion_numero(p):
+	''' operacion1 : inmediato'''
 	p[0] = p[1]
 
 # metodo auxiliar para poder factorizar mas facil
 def p_opestring(p):
 	'''opestring : STRING
 				 | operacion '''
-	p[0] = p[1]
-
-# reconoce cuando se solicita imprimir algo en consola
-def p_imprime(p):
-	'''imprime : PRINT PARENA opestring PARENC '''
-	p[0] = p[1] + p[2] + p[3] + p[4]
-
-def p_imprimetab(p):
-	'''imprimetab : tabs imprime '''
 	p[0] = p[1]
 
 # reconoce 2 o mas parametros
@@ -269,14 +209,19 @@ def p_parametros(p):
 				  | empty'''
 	p[0] = p[1]
 
+# ---------------------------------------------------------------------------------------
+# |		De aqui para abajo son cosas que pueden aparecer en una linea del codigo		|
+# ---------------------------------------------------------------------------------------
+
+# reconoce cuando se solicita imprimir algo en consola
+def p_imprime(p):
+	'''imprime : PRINT PARENA opestring PARENC '''
+	p[0] = p[1] + p[2] + p[3] + p[4]
+
 # reconoce comentarios y comentarios identados
 def p_comentario(p):
 	'''comentario : COMENT STRING '''
 	p[0] = p[1] + ' ' + p[2]
-
-def p_comentariotab(p):
-	'''comentariotab : tabs comentario '''
-	p[0] = p[0] + p[2]
 
 # reconoce la instruccion para retornar de una funcion
 # puede retornar operaciones o strings
@@ -287,12 +232,18 @@ def p_return(p):
 # estructura general de un procedimiento
 def p_procedimiento(p):
 	'''procedimiento : PROC ID PARENA parametros PARENC '''
-	p[0] = p[1] + ' ' + p[2] + ' ' + p[3] + p[4] +  p[5]
+	p[0] = p[1] + ' ' + p[2] + p[3] + p[4] + p[5] + ':'
+	global m_tabs
+	m_tabs += 1
 
 # estructura de una asignacion a una variable
 def p_asignacion(p):
 	'''asignacion : VAR ID ASIG opestring'''
 	p[0] = p[2] + p[3] + p[4]
+
+def p_asignacion_sindeclaracion(p):
+	'''asignacion : ID ASIG opestring'''
+	p[0] = p[1] + p[2] + p[2]
 
 # estructura de un condicional (if / else)
 def p_condicionif(p):
@@ -301,35 +252,52 @@ def p_condicionif(p):
 
 def p_condicionelse(p):
 	'''condicionelse : ELSE'''
-	p[0] = p[1] + ':'
+	global m_tabs
+	m_tabs -= 2
+	p[0] = add_tabs() + p[1] + ':'
+	m_tabs += 1
 
 def p_condicion(p):
 	'''condicion : condicionif
 				 | condicionelse'''
 	p[0] = p[1]
+	global m_tabs
+	m_tabs += 1
 
 # estructura de un ciclo while
 def p_ciclowhile(p):
 	'''ciclowhile : WHILE PARENA operacion PARENC'''
 	p[0] = p[1] + p[2] + p[3] + p[4] + ':'
+	global m_tabs
+	m_tabs += 1
 
 # estructura de un ciclo for
 def p_ciclofor(p):
 	'''ciclofor : FOR ID IN ID'''
 	p[0] = p[1] + p[2] + p[3] + p[4] + ':'
+	global m_tabs
+	m_tabs += 1
 
 # estructura de una operacion unaria
 def p_operacionunaria(p):
 	'''opeunaria : OPUNA ID '''
 	p[0] = p[1] + p[2]
 
-# estructura general de una instruccion que puede ser:
+# reconoce la estructura de un "input" en el pseudocodigo
+def p_preguntar(p):
+	'''preguntar : ASIG ASK PARENA STRING PARENC '''
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
+
+# estructura general de una linea de programa que puede ser:
 # 	- un procedimiento (metodo / funcion)
 # 	- una asignacion
 #	- operaciones unarias
 # 	- un condicional (if / else)
 #	- un ciclo while
 # 	- un ciclo for
+#	- un comentario
+#	- imprimir
+#	- preguntar al usuario
 # ademas acepta instrucciones que estan identadas
 def p_instruccion(p):
 	''' instruccion : procedimiento
@@ -338,27 +306,33 @@ def p_instruccion(p):
 					| ciclowhile
 					| ciclofor
 					| opeunaria
-					| return'''
-	p[0] = p[1]
-
-def p_instrucciontab(p):
-	''' instrucciontab : tabs instruccion '''
-	p[0] = p[0] + p[2]
+					| return
+					| comentario
+					| imprime
+					| preguntar'''
+	global m_tabs
+	p[0] = add_tabs() + p[1]
 
 def p_error(p):
 	global m_totLineas
 	global m_reporte
 	if p:
-		m_reporte +="Error de sintaxis en " + str(p.value) +". En la linea "+str(m_totLineas)
+		m_reporte +="Error de sintaxis en " + str(p.value) + ". En la linea " + str(m_totLineas)
 		print(m_reporte)
 	else:
 		m_reporte += "Error en "+str(m_totLineas)
 		print(m_reporte)
 
-import ply.yacc as yacc
 yacc.yacc()
 # programa
 
-sourceFile = open('prueba.txt', 'r')
-for i in archivo:
-	yacc.parse(i)
+sfname = ""
+sfname = str(raw_input('Ingrese el nombre del archivo que desea convertir a python> '))
+sourceFile = open(sfname, 'r')
+for line in sourceFile:
+	yacc.parse(line)
+sfname = str(raw_input('Ingrese el nombre con el que quiere guardar el archivo> '))
+file = open(sfname+".py", 'w')
+file.write(m_result)
+file.close()
+sourceFile.close()
