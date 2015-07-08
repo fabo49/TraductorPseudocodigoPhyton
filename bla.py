eb7fa621684211adfb3e63876f398730b0b68962
Fabian Rodriguez <farodriguez.49@gmail.com>
2015-07-08 17:14:03 -0600

    Se elimino el modulo innecesario

---
+++ bla.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------
+# Programa traducido de pseudocodigo a Python 
+# por un compilador hecho en el curso de Automatas y Compiladores
+# de la Universidad de Costa Rica por los estudiantes:
+#	- Fabian Rodriguez Obando
+# 	- Abraham Vega Delgado
+# Profesor: Luis Quesada
+# Fecha de conversion: 07/08/15
+# ---------------------------------------------------------------------
+
+def main():
+	x=32
+	# 'Prueba de comentario'
+	if(x==32):
+		print('Todo bien')
+	
